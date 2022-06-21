fbfae7c56e5085b5eb494ab08c06c7a46799e322
ghusainova <ghusainova00@gmail.com>
2022-06-21 19:37:57 +0300

    update lesson02

---
+++ script/lesson02.js
@@ -0,0 +1,9 @@
+let money = 3422;
+let income = 5050;
+let addExpenses = 'интернет, такси, коммуналка';
+let deposit = 10100;
+let mission = 121000;
+let period = 10;
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
