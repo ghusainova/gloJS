f26118b422dd785ab7cbf226a2f1ea40732c3f44
ghusainova <ghusainova00@gmail.com>
2022-06-21 18:24:17 +0300

    lesson1 update

--- script/lesson1.js
+++ script/lesson1.js
@@ -1,1 +1,10 @@
-'use strict'
+let money = 5000;
+let income = 5000;
+let addExpenses = 500;
+let deposit = 10000;
+const mission = 'Изучить JavaScript на курсе';
+const period = '2 месяца';
+
+alert('Привет!');
+
+console.log('Курс JavaScript');
